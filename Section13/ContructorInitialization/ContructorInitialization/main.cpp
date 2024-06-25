9e8b98f87fd1bfea2131f73bcbe9a7123b8133ca
Jobson Almeida <jobsonbox@gmail.com>
2024-06-25 15:47:24 -0300

    constructor initialization lists completed

--- Section13/ContructorInitialization/ContructorInitialization/main.cpp
+++ Section13/ContructorInitialization/ContructorInitialization/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class Player
 {
 private:
-    std::string name{ "XXXXXXX" };
+    std::string name;
     int health;
     int xp;
 public:
@@ -19,20 +19,17 @@ public:
 };
 
 Player::Player()
-    : name{ "None" }, health{ 0 }, xp{ 0 }
 {
+    name = "None";
+    health = 0;
+    xp = 0;
 }
 
-Player::Player(std::string name_val)
-    : name{ name_val }, health{ 0 }, xp{ 0 }
-{
-}
-
-Player::Player(std::string name_val, int health_val, int xp_val)
-    : name{ name_val }, health{ health_val }, xp{ xp_val }
-{
-
-}
+//Player::Player() : name{"None"}, health{0}, xp{0}{
+//    std::cout << name;
+//}
+Player::Player(std::string name_val) : name{ name_val }, health{ 0 }, xp{0} {}
+Player::Player(std::string name_val, int health_val, int xp_val) : name{ name_val }, health{ health_val }, xp{xp_val} {}
 
 int main()
 {
