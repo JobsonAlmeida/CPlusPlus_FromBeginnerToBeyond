7a22a912858eddda3f0298bc61825f002a344b65
Jobson Almeida <jobsonbox@gmail.com>
2024-06-25 16:32:57 -0300

    delegating construcotr finished

--- Section13/DelegatingConstructors/DelegatingConstructors/main.cpp
+++ Section13/DelegatingConstructors/DelegatingConstructors/main.cpp
@@ -19,9 +19,20 @@ public:
 };
 
 
+Player::Player() : Player("None", 0, 0 ) {
+    cout << "No-args constructor" << endl;
+}
+
+Player::Player(std::string name_val) : Player(name_val, 0, 0){
+    cout << "One-args constructor" << endl;
+}
+
+Player::Player(std::string name_val, int health_val, int xp_val) : name{ name_val }, health{ health_val }, xp{ xp_val } {
+    cout << "Three-args constructor" << endl;
+}
+
 int main()
 {
-
     Player empty;
     Player frank{ "Frank" };
     Player villain{ "Villain", 100, 55 };
