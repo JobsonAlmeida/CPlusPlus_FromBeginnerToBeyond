c7ef51227abe1f2c333ac40c7a3dcc414a265c2c
Jobson Almeida <jobsonbox@gmail.com>
2024-06-25 19:45:14 -0300

    modified to the begin

--- Section13/Project13/Project13/CopyConstructor.cpp
+++ Section13/Project13/Project13/CopyConstructor.cpp
@@ -16,10 +16,7 @@ public:
     int get_health() { return health; }
     int get_xp() { return xp; }
     Player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
-    // Copy constructor
-    Player(const Player& source);
-    // Destructor
-    ~Player() { cout << "Destructor called for: " << name << endl; }
+
 };
 
 Player::Player(std::string name_val, int health_val, int xp_val)
@@ -28,28 +25,9 @@ Player::Player(std::string name_val, int health_val, int xp_val)
     cout << "Three-args constructor for " + name << endl;
 }
 
-Player::Player(const Player& source)
-// : name(source.name), health(source.health), xp{source.xp} {
-    : Player{ source.name, source.health, source.xp }
-{
-    cout << "Copy constructor - made copy of: " << source.name << endl;
-}
-
-void display_player(Player p)
-{
-    cout << "Name: " << p.get_name() << endl;
-    cout << "Health: " << p.get_health() << endl;
-    cout << "XP: " << p.get_xp() << endl;
-}
-
 int main()
 {
-    Player empty{ "XXXXXX", 100, 50 };
-
-    Player my_new_object{ empty };
-
-    display_player(empty);
-
+    Player empty;
     Player frank{ "Frank" };
     Player hero{ "Hero", 100 };
     Player villain{ "Villain", 100, 55 };
