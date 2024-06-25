8e6e48082ea35ba076b016f2e93ec135984a1e05
Jobson Almeida <jobsonbox@gmail.com>
2024-06-25 19:39:25 -0300

    concluded

--- Section13/Project13/Project13/main.cpp
+++ Section13/Project13/Project13/main.cpp
@@ -12,22 +12,10 @@ private:
     int health;
     int xp;
 public:
-    // Overloaded Constructors
+   
+    Player(std::string name_val = "None", int health_val = 0, int xp_val = 0 );
     Player();
-    Player(std::string name_val);
-    Player(std::string name_val, int health_val, int xp_val);
 };
-
-
-Player::Player() : Player("None", 0, 0)
-{
-    cout << "No-args constructor" << endl;
-}
-
-Player::Player(std::string name_val) : Player(name_val, 0, 0)
-{
-    cout << "One-args constructor" << endl;
-}
 
 Player::Player(std::string name_val, int health_val, int xp_val) : name{ name_val }, health{ health_val }, xp{ xp_val }
 {
@@ -38,6 +26,7 @@ int main()
 {
     Player empty;
     Player frank{ "Frank" };
+    Player hero("Hero", 100);
     Player villain{ "Villain", 100, 55 };
 
     return 0;
