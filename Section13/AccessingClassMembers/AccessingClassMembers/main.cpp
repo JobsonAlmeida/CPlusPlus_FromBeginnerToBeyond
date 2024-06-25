f47e13611b5791942992bbb984c4b597ca7665e0
Jobson Almeida <jobsonbox@gmail.com>
2024-06-25 10:05:19 -0300

    completed methods definiton inside the class (inline) and outside from the class using the scope operator

--- Section13/AccessingClassMembers/AccessingClassMembers/main.cpp
+++ Section13/AccessingClassMembers/AccessingClassMembers/main.cpp
@@ -6,16 +6,13 @@ using namespace std;
 
 class Player
 {
-
-public:
+private:
 	string name;
 	int health;
 	int xp;
 
-	void talk(string text_to_say)
-	{
-		cout << name << " says " << text_to_say << endl;
-	}
+public:
+	void talk(string text_to_say){cout << name << " says " << text_to_say << endl;}
 	bool is_dead();
 
 };
@@ -26,41 +23,72 @@ public:
 	string name;
 	double balance;
 
-	bool deposit(double bal) { 
-		balance += bal; cout << "In deposit: " << endl; return true;
-	};
-	bool withdraw(double bal)
-	{
-		balance -= bal; cout << "In withdraw: " << endl; return true;
-	};
+public: 
+	void set_balance(double bal) { 
+		balance = bal; 
+	}
+	double get_balance() { return balance; }
+
+	void set_name(string n);
+	string get_name();
+
+	bool deposit(double amount);
+	bool withdraw(double amount);
 
 };
 
-int main()
+void Account::set_name(string n)
+{
+	name = n;
+}
+
+void set_name(string n)
 {
 
+}
+
+bool Account::deposit(double amount)
+{
+
+	//some logic business
+
+	balance += amount;
+	return true;
+}
+
+bool Account::withdraw(double amount)
+{
+	if (balance - amount >= 0)
+	{
+		balance -= amount;
+		return true; 
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int main()
+{
 	Account frank_account;
-	frank_account.name = "Frank'account";
-	frank_account.balance = 5000.000;
+	frank_account.set_name("Frank's account");
+	frank_account.set_balance(1000);
 
-	frank_account.deposit(1000);
-	frank_account.withdraw(500);
-		
-	Player frank;
-	Player hero;
+	if (frank_account.deposit(200.00))
+		cout << "Deposit OK" << endl;
+	else
+		cout << "Deposit Not allowed" << endl;
 
-	frank.name = "Frank";
-	frank.health = 10;
-	frank.xp = 12;
+	if (frank_account.withdraw(500))
+		cout << "Withdraw OK" << endl;
+	else
+		cout << "Not sufficient funds" << endl;
 
-	frank.talk("Hi there");
-
-	Player *enemy = new Player;
-	(*enemy).name = "Enemy";
-	(*enemy).health = 100;
-	enemy->xp = 15;
-
-	enemy->talk("I will destroy you");
+	if (frank_account.withdraw(1500.0))
+		cout << "Withdraw OK" << endl;
+	else
+		cout << "Not sufficient funds" << endl;
 
 	return 0;
 }
