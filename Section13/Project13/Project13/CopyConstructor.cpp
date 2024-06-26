ddf69ea7b8e2a4e528011de652250f218c425d58
Jobson Almeida <jobsonbox@gmail.com>
2024-06-25 21:27:17 -0300

    completed copy constructor

--- Section13/Project13/Project13/CopyConstructor.cpp
+++ Section13/Project13/Project13/CopyConstructor.cpp
@@ -17,6 +17,12 @@ public:
     int get_xp() { return xp; }
     Player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
 
+    //Copy Constructor
+    Player(const Player& source);
+
+    //Destructor
+    ~Player() { cout << "Destructor called for: " << name << endl; }
+
 };
 
 Player::Player(std::string name_val, int health_val, int xp_val)
@@ -25,9 +31,28 @@ Player::Player(std::string name_val, int health_val, int xp_val)
     cout << "Three-args constructor for " + name << endl;
 }
 
+Player::Player(const Player &source) 
+    //: name(source.name), health(source.health), xp(source.xp){
+    : Player(source.name, source.health, source.xp){
+    cout << "Copy constructor - made copy of: " << source.name << endl;
+}
+
+void display_player(Player p)
+{
+    cout << "Name: " << p.get_name() << endl;
+    cout << "Health: " << p.get_health() << endl;
+    cout << "XP: " << p.get_xp() << endl;
+}
+
+
 int main()
 {
-    Player empty;
+    Player empty{"XXXXXX", 100, 50};
+
+    Player my_new_object{ empty };
+
+    display_player(empty);
+
     Player frank{ "Frank" };
     Player hero{ "Hero", 100 };
     Player villain{ "Villain", 100, 55 };
