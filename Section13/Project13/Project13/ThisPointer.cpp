e9766a6f1feec07721ecd34bb32c114cb0617273
Jobson Almeida <jobsonbox@gmail.com>
2024-06-27 16:40:09 -0300

    static class member completed

--- Section13/Project13/Project13/ThisPointer.cpp
+++ Section13/Project13/Project13/ThisPointer.cpp
@@ -1,26 +1,26 @@
-
-
-class Account{
-
-	float balance;
-
-public:
-	void set_balance(double balance);
-
-};
-
-void Account::set_balance(double balance)
-{
-	balance = balance;
-}
-
-
-int main()
-{
-
-	Account acc;
-
-	acc.set_balance(200.00);
-
-	return 0;
-}
+//
+//
+//class Account{
+//
+//	float balance;
+//
+//public:
+//	void set_balance(double balance);
+//
+//};
+//
+//void Account::set_balance(double balance)
+//{
+//	balance = balance;
+//}
+//
+//
+//int main()
+//{
+//
+//	Account acc;
+//
+//	acc.set_balance(200.00);
+//
+//	return 0;
+//}
