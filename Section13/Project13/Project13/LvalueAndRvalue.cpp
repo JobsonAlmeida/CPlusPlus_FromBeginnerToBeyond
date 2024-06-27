3040444ad8ddcb75f43a112286b4d1e40cfadcfc
Jobson Almeida <jobsonbox@gmail.com>
2024-06-27 11:46:02 -0300

    completed move constructor

--- Section13/Project13/Project13/LvalueAndRvalue.cpp
+++ Section13/Project13/Project13/LvalueAndRvalue.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-
-int x = 100;
-
-int Func(int &&num)
-{
-	std::cout << "num + 1: " << num + 1;
-	return num + 1;
-
-}
-
-int main()
-{
-	Func(100);
-}
-
-
+//#include <iostream>
+//
+//int x = 100;
+//
+//int Func(int &&num)
+//{
+//	std::cout << "num + 1: " << num + 1;
+//	return num + 1;
+//
+//}
+//
+//int main()
+//{
+//	Func(100);
+//}
+//
+//
