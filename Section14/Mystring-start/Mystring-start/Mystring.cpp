ea660b1d8357827a410b7f8e18652e910cd25f49
Jobson Almeida <jobsonbox@gmail.com>
2024-07-04 09:58:10 -0300

    completed copy assignment operator

---
+++ Section14/Mystring-start/Mystring-start/Mystring.cpp
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include <string.h>
+#include <iostream>
+#include "Mystring.h"
+
+Mystring::Mystring() : str{ nullptr }
+{
+	str = new char[1];
+	*str = '\0';
+}
+
+Mystring::Mystring(const char* s) : str{ nullptr }
+{
+	if (s == nullptr)
+	{
+		str = new char[1];
+		*str = '\0';
+	}
+	else
+	{
+		str = new char[std::strlen(s) + 1]; // takes the leght of s and sums 1 on that. 
+		std::strcpy(str, s); //strcpy copies s to str. 
+	}
+}
+
+Mystring::Mystring(const Mystring& source) : str{ nullptr }
+{
+	str = new char[std::strlen(source.str) + 1];
+	std::strcpy(str, source.str);
+}
+
+//Remember, this you're responsible for de-allocating the memory that you allocate.
+Mystring::~Mystring()
+{
+	delete [] str;
+}
+
+Mystring& Mystring::operator=(const Mystring& rhs)
+{
+	std::cout << "Copy assignment" << std::endl;
+
+	if (this == &rhs)
+	{
+		return *this;
+	}
+
+	delete this->str;
+
+	str = new char[std::strlen(rhs.str) + 1]; 
+
+	std::strcpy(this->str, rhs.str);
+
+	return *this;
+
+}
+
+void Mystring::display() const
+{
+	std::cout << str << ":" << get_length() << std::endl;
+}
+
+int Mystring::get_length() const
+{
+	return std::strlen(str);
+}
+
+const char* Mystring::get_str() const
+{
+	return str;
+}
