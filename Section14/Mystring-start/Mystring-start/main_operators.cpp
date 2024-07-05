653e37418bd5d6e0451561aff50f26d4b50a3fd5
Jobson Almeida <jobsonbox@gmail.com>
2024-07-05 18:46:19 -0300

    debugged overloading operators as member functiosn

--- Section14/Mystring-start/Mystring-start/main_operators.cpp
+++ Section14/Mystring-start/Mystring-start/main_operators.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-#include <vector>
-#include "Mystring.h"
-
-using namespace std;
-
-int main()
-{
-	Mystring larry{ "Larry" };
-	Mystring moe{ "Moe" };
-
-	Mystring stooge = larry;
-	larry.display();
-	moe.display();
-
-	cout << (larry == moe) << endl;
-	cout << (larry == stooge) << endl;
-
-	larry.display();
-	Mystring larry2 = -larry;
-	larry2.display();
-
-	Mystring stooges = larry + "Moe";
-	//Mystring stooges = "Larry" + moe; //compiler error
-
-	Mystring two_stooges = moe + " " + "Larry";
-	two_stooges.display();
-
-	Mystring trhee_stooges = moe + " " + larry + " " + "Curly";
-	three_stooges.display();
-
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include "Mystring.h"
+//
+//using namespace std;
+//
+//int main()
+//{
+//	Mystring larry{ "Larry" };
+//	Mystring moe{ "Moe" };
+//
+//	Mystring stooge = larry;
+//	larry.display();
+//	moe.display();
+//
+//	cout << (larry == moe) << endl;
+//	cout << (larry == stooge) << endl;
+//
+//	larry.display();
+//	Mystring larry2 = -larry;
+//	larry2.display();
+//
+//	Mystring stooges = larry + "Moe";
+//	//Mystring stooges = "Larry" + moe; //compiler error
+//
+//	Mystring two_stooges = moe + " " + "Larry";
+//	two_stooges.display();
+//
+//	Mystring trhee_stooges = moe + " " + larry + " " + "Curly";
+//	three_stooges.display();
+//
+//	return 0;
+//}
