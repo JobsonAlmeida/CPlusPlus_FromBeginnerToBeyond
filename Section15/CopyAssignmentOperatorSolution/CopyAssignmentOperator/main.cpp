0407ea1c83e040de4c3be38c884a51e632c918e0
Jobson Almeida <jobsonbox@gmail.com>
2024-07-16 15:49:47 -0300

    copy, move constructors and assignment operators in derived classes  completed

---
+++ Section15/CopyAssignmentOperatorSolution/CopyAssignmentOperator/main.cpp
@@ -0,0 +1,84 @@
+// Section 15
+// Copy constructor and operator=
+#include <iostream>
+
+using namespace std;
+
+class Base {
+private:
+    int value;
+public:
+    Base()
+        : value{ 0 }
+    {
+        cout << "Base no-args constructor" << endl;
+    }
+    Base(int x)
+        : value{ x }
+    {
+        cout << "int Base constructor" << endl;
+    }
+    Base(const Base& other)
+        : value{ other.value }
+    {
+        cout << "Base copy constructor" << endl;
+    }
+
+    Base& operator=(const Base& rhs)
+    {
+        cout << "Base operator=" << endl;
+        if (this == &rhs)
+            return *this;
+        value = rhs.value;
+        return *this;
+    }
+
+    ~Base() { cout << "Base destructor" << endl; }
+};
+
+class Derived : public Base {
+private:
+    int doubled_value;
+public:
+    Derived() :
+        Base{}
+    {
+        cout << "Derived no-args constructor " << endl;
+    }
+    Derived(int x)
+        : Base{ x }, doubled_value{ x * 2 }
+    {
+        cout << "int Derived constructor" << endl;
+    }
+    Derived(const Derived& other)
+        : Base(other), doubled_value{ other.doubled_value }
+    {
+        cout << "Derived copy constructor" << endl;
+    }
+
+    Derived& operator=(const Derived& rhs)
+    {
+        cout << "Derived operator=" << endl;
+        if (this == &rhs)
+            return *this;
+        Base::operator=(rhs); //The equal assignment operator from Base is calles so that is can initialize some properties
+        doubled_value = rhs.doubled_value; // others propeeties that are not initialized by the assignment operator from the Base class are initialized individualy
+        return *this;
+    }
+    ~Derived() { cout << "Derived destructor " << endl; }
+};
+
+
+int main()
+{
+   Base b {100};   // Overloaded constructor
+   Base b1 {b};    // Copy constructor
+   b = b1;          //   Copy assignment
+
+    Derived d{ 100 };    // Overloaded constructor
+    Derived d1{ d };     // Copy constructor
+    d = d1;                // Copy assignment
+
+    return 0;
+}
+
