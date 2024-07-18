a054f2e0697498cb2ec6a474f83e9fd17731134f
Jobson Almeida <jobsonbox@gmail.com>
2024-07-18 12:00:20 -0300

    Using a base class pointer with pointers

---
+++ Section16/Section16_Solution/Section16_Project/main2.cpp
@@ -0,0 +1,69 @@
+
+#include <iostream>
+
+// This class uses dynamic polymorphism for the withdraw method
+// We'll learn about virtual functions in the next video
+class Account {
+public:
+    virtual void withdraw(double amount)
+    {
+        std::cout << "In Account::withdraw" << std::endl;
+    }
+    virtual ~Account() {}
+};
+
+class Checking : public Account {
+public:
+    virtual void withdraw(double amount)
+    {
+        std::cout << "In Checking::withdraw" << std::endl;
+    }
+
+    virtual ~Checking() {}
+};
+
+class Savings : public Account {
+public:
+    virtual void withdraw(double amount)
+    {
+        std::cout << "In Savings::withdraw" << std::endl;
+    }
+    virtual ~Savings() {}
+};
+
+class Trust : public Account {
+public:
+    virtual void withdraw(double amount)
+    {
+        std::cout << "In Trust::withdraw" << std::endl;
+    }
+    virtual ~Trust() {}
+};
+
+
+int main()
+{
+	std::cout << "\n === Pointers === " << std::endl;
+
+	Account *p1 = new Account();
+	Account *p2 = new Savings();
+	Account *p3 = new Checking();
+	Account *p4 = new Trust();
+
+	p1->withdraw(1000);
+	p2->withdraw(1000);
+	p3->withdraw(1000);
+	p4->withdraw(1000);
+
+
+
+	std::cout << "\n === Clean up === " << std::endl;
+	delete p1;
+	delete p2;
+	delete p3;
+	delete p4;
+
+
+
+
+}
