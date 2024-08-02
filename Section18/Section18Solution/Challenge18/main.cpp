a69b294d80de3e1e8ec5c759e3054f9089f95dd9
Jobson Almeida <jobsonbox@gmail.com>
2024-08-02 09:13:25 -0300

    challenge completed

--- Section18/Section18Solution/Challenge18/main.cpp
+++ Section18/Section18Solution/Challenge18/main.cpp
@@ -5,10 +5,44 @@
 #include "Savings_Account.h"
 #include "Trust_Account.h"
 #include "Account_Util.h"
+#include "IllegalBalanceException.h"
+#include "InsufficientFundsException.h"
 
 using namespace std;
 
 int main() {
+
+    std::unique_ptr<Account> moes_account;
+    std::unique_ptr<Account> larrys_account;
+
+    try
+    {
+        larrys_account = std::make_unique<Savings_Account>("Larry", -2000);
+        std::cout << *larrys_account << std::endl;
+    }
+    catch (const IllegalBalanceException& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+    }
+
+    try
+    {
+        moes_account = std::make_unique<Savings_Account>("Moe", 1000);
+        std::cout << *moes_account << std::endl;
+        moes_account->withdraw(500);
+        std::cout << *moes_account << std::endl;
+        moes_account->withdraw(1000);
+        std::cout << *moes_account << std::endl;
+    }
+    catch (const IllegalBalanceException& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+    }
+    catch (const InsufficientFundsException &ex)
+    {
+        std::cerr << ex.what() << std::endl;
+    }
+
     // test your code here
     std::cout << "Program completed successfully" << std::endl;
     return 0;
