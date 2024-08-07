1f66e86ddecb802c82341ddf3adff3bbc2dfb118
Jobson Almeida <jobsonbox@gmail.com>
2024-08-07 10:54:42 -0300

    challenge 1 completed

--- Section19/Section19_Solution/Challenge_1/main.cpp
+++ Section19/Section19_Solution/Challenge_1/main.cpp
@@ -56,17 +56,55 @@ int main()
         }
     };
 
-    // Unformatted display so you can see how to access the vector elements
-    std::cout << tours.title << std::endl;
-    for(auto country : tours.countries) {   // loop through the countries
-        std::cout << country.name << std::endl;
-        for(auto city : country.cities) {       // loop through the cities for each country
-            std::cout << "\t" << city.name 
-                          << "\t" << city.population 
-                          << "\t" << city.cost 
-                          << std::endl;
+
+    const int total_width{ 70 };
+    const int field1_width{ 20 };
+    const int field2_width{ 20 };
+    const int field3_width{ 15 };
+    const int field4_width{ 15 };
+
+    int title_length = tours.title.length();
+
+    std::cout << std::setw((total_width - title_length) / 2) << "" << tours.title << std::endl;
+    std::cout << std::endl;
+    std::cout << std::setw(field1_width) << std::left << "Country"
+        << std::setw(field2_width) << std::left << "City"
+        << std::setw(field3_width) << std::right << "Population"
+        << std::setw(field4_width) << std::right << "Price"
+        << std::endl;
+
+    std::cout << std::setw(total_width)
+        << std::setfill('-')
+        << ""
+        << std::endl;
+
+    std::cout << std::setfill(' ');
+    std::cout << std::setprecision(2) << std::fixed;
+
+    for (Country country : tours.countries)
+    {
+        for (size_t i = 0; i < country.cities.size(); i++)
+        {
+            std::cout << std::setw(field1_width) << std::left << ((i == 0) ? country.name : "")
+                << std::setw(field2_width) << std::left << country.cities.at(i).name
+                << std::setw(field3_width) << std::right << country.cities.at(i).population
+                << std::setw(field4_width) << std::right << country.cities.at(i).cost
+                << std::endl;
         }
     }
+
+
+    // Unformatted display so you can see how to access the vector elements
+    //std::cout << tours.title << std::endl;
+    //for(auto country : tours.countries) {   // loop through the countries
+    //    std::cout << country.name << std::endl;
+    //    for(auto city : country.cities) {       // loop through the cities for each country
+    //        std::cout << "\t" << city.name 
+    //                      << "\t" << city.population 
+    //                      << "\t" << city.cost 
+    //                      << std::endl;
+    //    }
+    //}
 
     std::cout << std::endl << std::endl;
     return 0;
