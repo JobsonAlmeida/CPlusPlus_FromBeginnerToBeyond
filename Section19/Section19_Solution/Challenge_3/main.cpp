6ecf706e615026f5746ac3d97c81261fde37c251
Jobson Almeida <jobsonbox@gmail.com>
2024-08-11 12:55:56 -0300

    challenge 3 completed

---
+++ Section19/Section19_Solution/Challenge_3/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+
+
+bool find_substring(const std::string& word_to_find, const std::string& word_read)
+{
+	std::size_t found = word_read.find(word_to_find);
+
+	if (found != std::string::npos)
+		return true;
+	else
+		return false;
+
+}
+
+int main()
+{
+	std::ifstream in_file{};
+	std::string word_to_find{};
+	std::string word_read{};
+	int word_count{ 0 };
+	int match_count{ 0 };
+
+	in_file.open("./romeoandjuliet.txt");
+	if (!in_file)
+	{
+		std::cerr << "Problem opening file" << std::endl;
+		return 1;
+	}
+
+	std::cout << "Enter the substring to search for: ";
+	std::cin >> word_to_find;
+
+	while (in_file >> word_read)
+	{
+		++word_count;
+		if (find_substring(word_to_find, word_read))
+		{
+			++match_count;
+		}
+
+	}
+
+	std::cout << word_count << " words were searched..." << std::endl;
+	std::cout << "The substring " << word_to_find << " was found " << match_count << " times." << std::endl;
+
+	return 0;
+}
